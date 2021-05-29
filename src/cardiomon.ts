833b62eec3029f2a15a2761c191166493dac3114
p4stoboy <b1gh@protonmail.com>
2021-05-29 23:02:37 +1000

    Working.

--- src/cardiomon.ts
+++ src/cardiomon.ts
@@ -9,6 +9,8 @@ type HSLColor = {
 
 export type JSONMon = {
     x_limit: number,
+    level: number,
+    seed: number,
     c1: number[],
     c2: number[],
     c3: number[],
@@ -33,63 +35,75 @@ export class Cardiomon {
         this.cells = [];
         this.c1 = {
             hue: this.rng.random(0, 359),
-            sat: this.rng.random(0.5, 0.8, 1),
-            lig: 0.8
+            sat: 1,
+            lig: this.rng.random(0.5, 0.8, 1)
         };
         this.c2 = {
             hue: this.rng.random(0, 359),
-            sat: this.rng.random(0.5, 0.8, 1),
-            lig: 0.8
+            sat: 1,
+            lig: this.rng.random(0.5, 0.8, 1)
         };
         this.c3 = {
             hue: this.c1.hue,
-            sat: this.c1.sat - 0.3,
-            lig: 0.8
+            sat: 1,
+            lig: this.c1.lig - 0.3
         };
 
         this.do_cells();
     }
 
+    // seed draw coordinates / run symmetry / iterate to isolate cells & determine colour
     do_cells = (): void => {
         const half = this.x_limit / 2;
 
         for (let i = 0; i < this.x_limit; i++) {
-             this.cells[i] = [];
+            this.cells[i] = [];
         }
 
         for (let y = 0; y < half; y++) {
-            for (let x = 0; x < half; x++) {
-                this.cells[x][y] = (x === 0 || x === this.cells.length - 1 || y === 0 || y === this.cells.length - 1) ? 0 : this.rng.random(0, 1);
+            for (let x = 0; x < this.x_limit; x++) {
+                this.cells[x][y] = (x === 0 || y === 0 || x === this.x_limit - 1 || y === this.x_limit - 1) ? 0 : this.rng.random(0, 1);
                 // symmetry
-                this.cells[this.cells.length - 1 - x][y] = this.cells[x][y];
+                this.cells[x][this.x_limit - 1 - y] = this.cells[x][y];
             }
         }
 
         for (let y = 0; y < this.x_limit; y++) {
             for (let x = 0; x < this.x_limit; x++) {
-                if (this.is_border(this.cells, x, y)) {
+                if (this.cells[x][y] === 0 && this.is_border(this.cells, x, y, 1)) {
                     this.cells[x][y] = 2;
+                }
+            }
+        }
+        for (let y = 0; y < this.x_limit; y++) {
+            for (let x = 0; x < this.x_limit; x++) {
+                if (this.cells[x][y] === 1 && this.is_border(this.cells, x, y, 2) === 4) {
+                    this.cells[x][y] = 3;
                 }
             }
         }
     }
 
-    is_border = (cells: number[][], x: number, y: number): boolean => {
-        if (cells[x][y] === 1) return false;
+    // do cells neighbouring this cell contain value $check
+    is_border = (cells: number[][], x: number, y: number, check: number): number => {
+        let count = 0;
         // left
-        if (x !== 0 && cells[x - 1][y] === 1) return true;
+        if (x !== 0 && cells[x - 1][y] === check) count++;
         //right
-        if (x !== cells.length - 1 && cells[x + 1][y] === 1) return true;
+        if (x !== cells.length - 1 && cells[x + 1][y] === check) count++;
         //up
-        if (y !== 0 && cells[x][y - 1] === 1) return true;
+        if (y !== 0 && cells[x][y - 1] === check) count++;
         //down
-        if (y !== cells.length - 1 && cells[x][y + 1] === 1) return true;
-        return false;
+        if (y !== cells.length - 1 && cells[x][y + 1] === check) count++;
+        return count;
     }
 
+    // convert to json string with 1d cells array for use on particle
     to_json = (): JSONMon => {
         return {
             x_limit: this.x_limit,
+            level: this.level,
+            seed: this.seed,
             c1: h2r(this.c1.hue, this.c1.sat, this.c1.lig),
             c2: h2r(this.c2.hue, this.c2.sat, this.c2.lig),
             c3: h2r(this.c3.hue, this.c3.sat, this.c3.lig),
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,54 @@
 import express from 'express';
 import lcg from 'tiny-rng';
-import { Cardiomon } from './classes/cardiomon';
+import db from 'better-sqlite3';
+import {Cardiomon, JSONMon} from './cardiomon';
+
+const sql = new db('cardiomon.db');
 const app = express();
 app.use(express.json());
+
 const port = 3000
+
 const rng = new lcg(0, Math.floor(Date.now() / 1000));
 
-app.post('/newmon', (req, res) => {
+sql.prepare(`CREATE TABLE IF NOT EXISTS cardiomon(id INTEGER PRIMARY KEY NOT NULL, device_id TEXT NOT NULL, 
+                     seed INTEGER NOT NULL, level INTEGER NOT NULL DEFAULT 1)`).run();
+
+const random_mon = (device_id: string): JSONMon => {
     const new_seed = rng.random(1, 10000);
+    sql.prepare(`INSERT INTO cardiomon(device_id, seed) VALUES(?, ?)`).run(device_id, new_seed);
     const mon = new Cardiomon(new_seed, 1);
-    const jsonMon = mon.to_json();
+    return mon.to_json();
+}
+
+// new mon level 1
+app.post('/newmon', (req, res) => {
+    console.log(`device: ${req.body.device_id} got a new mon.`);
+    res.send(JSON.stringify(random_mon(req.body.device_id)));
+});
+
+// return levelled up mon
+app.post('/levelup', (req, res) => {
+    const b = req.body;
+    console.log(`device: ${b.device_id} levelled up.`);
+    const seed = parseInt(b.seed);
+    const level = parseInt(b.level);
+    sql.prepare(`UPDATE cardiomon SET level = level + 1 WHERE device_id = ? AND seed = ? AND level = ?`).run(b.device_id, seed, level);
+    const jsonMon = new Cardiomon(seed, level + 1).to_json();
     res.send(JSON.stringify(jsonMon));
 });
 
-app.post('/levelup', (req, res) => {
-    const b = req.body;
-    const seed = parseInt(b.seed);
-    const level = parseInt(b.level);
-    const mon = new Cardiomon(seed, level);
-    const jsonMon = mon.to_json();
-    res.send(JSON.stringify(jsonMon));
+// reload mon if device shuts off
+app.post('/reload', (req, res) => {
+    console.log(`device: ${req.body.device_id} is reloading their mon.`);
+    const current_mon = sql.prepare(`SELECT * FROM cardiomon WHERE device_id = ? AND level < 5`).get(req.body.device_id);
+    if (current_mon) {
+        const jsonMon = new Cardiomon(current_mon.seed, current_mon.level).to_json();
+        res.send(JSON.stringify(jsonMon));
+    } else {
+        res.send(JSON.stringify(random_mon(req.body.device_id)));
+    }
+
 });
 
 app.listen(port, () => {
